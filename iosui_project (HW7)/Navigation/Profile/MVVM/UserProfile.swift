dd7716f46bb2abe5e0ac27f039fc63fc38ce6afa
Sergey <121692119+MSergey95@users.noreply.github.com>
2024-08-16 14:27:26 +0800

    Add files via upload
    The MVVM (Model-View-ViewModel) architecture was implemented in the “Profile” module of the Navigation application. A ProfileViewModel class was created to manage the data and logic for ProfileViewController, separating concerns and enhancing code maintainability. Additionally, memory management was verified by checking for potential leaks using Instruments.

---
+++ iosui_project (HW7)/Navigation/Profile/MVVM/UserProfile.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfile.swift
+//  Navigation
+//
+//  Created by Сергей Минеев on 8/16/24.
+//
+
+import Foundation
+
+struct UserProfile {
+    var name: String
+    var status: String
+}
