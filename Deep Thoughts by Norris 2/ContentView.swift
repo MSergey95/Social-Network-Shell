79f38d132d25128a029de7201e4bd9d9e9d636e0
Sergey <121692119+MSergey95@users.noreply.github.com>
2024-10-17 13:00:20 +0800

    Initial Commit

---
+++ Deep Thoughts by Norris 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deep Thoughts by Norris 2
+//
+//  Created by Сергей Минеев on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deep Thoughts by Norris 2/Deep_Thoughts_by_Norris_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Deep_Thoughts_by_Norris_2App.swift
+//  Deep Thoughts by Norris 2
+//
+//  Created by Сергей Минеев on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Deep_Thoughts_by_Norris_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
