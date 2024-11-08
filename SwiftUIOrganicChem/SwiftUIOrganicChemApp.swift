0d149a51338b97c16028124648c21d52ab3c519d
Sergey <121692119+MSergey95@users.noreply.github.com>
2024-11-08 13:47:22 +0800

    Initial Commit

---
+++ SwiftUIOrganicChem/SwiftUIOrganicChemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOrganicChemApp.swift
+//  SwiftUIOrganicChem
+//
+//  Created by Сергей Минеев on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOrganicChemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
