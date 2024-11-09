d519bfe78406f488f41edfb80c792ff64c671d6f
Sergey <121692119+MSergey95@users.noreply.github.com>
2024-11-09 15:13:00 +0800

    Added files for SwiftUIOrgChem project

---
+++ SwiftUIOrganicChem/Views /HelloView.swift
@@ -0,0 +1,23 @@
+//
+//  HelloView.swift
+//  SwiftUIOrganicChem
+//
+//  Created by Сергей Минеев on 11/8/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HelloView: View {
+    var body: some View {
+        Text("Hello world")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct HelloView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloView()
+    }
+}
